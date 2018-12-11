5fbc600cb71af85a1b04d9254cba6836b1e32f1a
cs286fall2018 <45797921+cs286fall2018@users.noreply.github.com>
2018-12-11 13:23:12 -0800

    Updated files for clean and split
    Updated files for clean and split

---
+++ SplitAndClean.py
@@ -0,0 +1,52 @@
+
+# coding: utf-8
+
+# # split_with_space(inputFilePath,outputFilePath, splitLength) - 
+
+# Function to return a splitted 136 character file at outputFilePath provided
+
+# In[68]:
+
+
+def split_with_space(inputFilePath,outputFilePath, splitLength):    
+    with open(inputFilePath, 'r') as f:          # filePath = 'alice_oz.txt', splitLength = 136
+        textData = f.read()                   
+    
+    textEach = ""                         # initialize
+    textWordCount = 0                  # initialize      
+    textWords = textData.split(" ")        # All words in the text
+    for word in textWords:
+        if(word!= ""):            
+            if((textWordCount + len(word))<splitLength):    # check if the new word added does not make length >136. If it does, we exclude it
+                textEach = textEach + " "+ word
+                textWordCount = textWordCount + len(word)
+            else:
+                with open(outputFilePath, 'a+') as f:     #outputFilePath = 'newFile.txt'
+                    f.write(textEach)
+                    f.write('\n')
+                textEach = word          # reset for the next line
+                textWordCount = len(word)   # reset for the next line
+
+split_with_space('alice_oz.txt','newFile.txt',136)
+
+
+# #  RemoveSpaces(inputFilePath,outputFilePath)
+
+# returns without space file at the outputFilePath location 
+
+# In[69]:
+
+
+def RemoveSpaces(inputFilePath,outputFilePath):    
+    with open(inputFilePath, 'r') as f:          # filePath = 'newFile.txt'
+        textData = f.readlines()
+    for t in textData:
+        s = t.replace(' ', '')
+        #print(len(s))
+        #print(s)
+        with open(outputFilePath, 'a+') as f:     #outputFilePath = 'finalWithotSpace.txt'
+            f.write(s)
+            f.write('\n')
+
+RemoveSpaces('newFile.txt', "finalWithotSpace.txt")
+
